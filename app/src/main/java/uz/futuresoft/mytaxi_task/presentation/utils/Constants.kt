71f7d471b270a73f7c485046e2194fb24f37f06d
Shohrux Ravshanbekov <shohrux.ravshanbekov001@gmail.com>
2024-08-21 15:40:16 +0500

    Project completed

---
+++ app/src/main/java/uz/futuresoft/mytaxi_task/presentation/utils/Constants.kt
@@ -0,0 +1,6 @@
+package uz.futuresoft.mytaxi_task.presentation.utils
+
+object Constants {
+    const val MAPBOX_STANDARD_LIGHT = "mapbox://styles/clever2001boy/clzv2m23s007q01r04dt4csou"
+    const val MAPBOX_STANDARD_DARK = "mapbox://styles/clever2001boy/clzv2k0tu00iu01qo9lwn6vmc"
+}
